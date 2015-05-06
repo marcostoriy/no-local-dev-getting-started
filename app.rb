3ab53f5b26b34e77f05e77c0057c0cae5dad36cc
marcostoriy <marcostoriy@gmail.com>
2015-05-06 03:14:18 -0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -17,16 +17,6 @@ get "/" do
   erb :home
 end
 
-
-class Contact < ActiveRecord::Base
-  self.table_name = 'connect.contact'
-end
-
-get "/contacts" do
-  @contacts = Contact.all
-  erb :index
-end
-
 get "/create" do
   CREATE_URL = 'https://connect.heroku.com/dashboard-next/create-connection'
   match = /(.*?)\.herokuapp\.com/.match(request.host)
